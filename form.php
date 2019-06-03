f19c02918207ec0f8498874a2cd1ad25347c5e86
oleg105 <dp131192pka@gmail.com>
2019-06-03 20:43:34 +0300

    ver2

--- form.php
+++ form.php
@@ -1,32 +1,43 @@
 <?php
 
-$parameters = ['operand1', 'operator', 'operand2'];
+$result = '';
+$operand1 = '';
+$operand2 = '';
+$operator = '';
 
-foreach ($parameters as $parameter) {
-    if (empty($_POST[$parameter])) {
-        die('element not found');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $parameters = ['operand1', 'operator', 'operand2'];
+
+    foreach ($parameters as $parameter) {
+        if (empty($_POST[$parameter])) {
+            die('element not found');
+        }
     }
+
+    $operand1 = floatval($_POST['operand1']);
+    $operand2 = floatval($_POST['operand2']);
+    $operator = $_POST['operator'];
+
+    switch ($operator) {
+        case '+':
+            $result = $operand1 + $operand2;
+            break;
+        case '-':
+            $result = $operand1 - $operand2;
+            break;
+        case '*':
+            $result = $operand1 * $operand2;
+            break;
+        case '/':
+            $result = $operand1 / $operand2;
+            break;
+        default:
+            die('wrong operator: ' . $operator);
+    }
+
+    //echo $operand1 . ' ' . $operator . ' ' . $operand2 . ' = ' . $result;
+
 }
-
-$operand1 = floatval($_POST['operand1']);
-$operand2 = floatval($_POST['operand2']);
-$operator = $_POST['operator'];
-
-switch ($operator) {
-    case '+':
-        $result = $operand1 + $operand2;
-        break;
-    case '-':
-        $result = $operand1 - $operand2;
-        break;
-    case '*':
-        $result = $operand1 * $operand2;
-        break;
-    case '/':
-        $result = $operand1 / $operand2;
-        break;
-    default:
-        die('wrong operator: '. $operator);
-}
-
-echo $operand1 . ' ' . $operator . ' ' . $operand2 . ' = ' . $result;
+include 'form.html';
